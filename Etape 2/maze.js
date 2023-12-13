a396d28249f6024c26bda3feda3e234fc3a022aa
Monique-Silva <monique.silva-ximenes@le-campus-numerique.fr>
2023-12-13 16:47:14 +0100

    fin de pseudo-code qui a priori doit marcher pour n'importe quel labyrinthe

---
+++ Etape 2/maze.js
@@ -0,0 +1,18 @@
+let numberOfSteps = 0;
+let win = false;
+let maze = [
+    ["S", "X", "0", "0", "0", "0", "0"],
+    ["0", "X", "0", "X", "X", "0", "X"],
+    ["0", "X", "0", "X", "G", "0", "0"],
+    ["0", "0", "0", "0", "X", "X", "0"],
+    ["0", "X", "0", "1", "0", "0", "0"],
+    ["0", "X", "0", "0", "0", "X", "0"],
+]
+
+function move(numberOfSteps, win, maze) {
+    if (maze[x] [y+1] = "0") {
+        maze[x] [y + 1] = "R";
+    }
+    else if (maze)
+}
+
